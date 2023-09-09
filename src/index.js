8bd7e9e97dea5080d7a07ccec4dc69d40825995b
F7MH2R <skyicef7@gmail.com>
2023-09-09 09:17:47 -0600

    shet

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Error from "./components/Error";
-import Contact from "./Contact";
+import Contact from "./components/Contact";
 
 ReactDOM.render(
   <HashRouter>
