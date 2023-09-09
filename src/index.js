4d40dc3ee882d3fe1f2379a426bc8de09b4d1f46
F7MH2R <skyicef7@gmail.com>
2023-09-08 20:38:29 -0600

    arreglando esta chet de los router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { HashRouter as ROUTER } from "react-router-dom";
 import App from "./App";
-import Error from "./components/Error";
+//import Error from "./components/Error";
 
 ReactDOM.render(
   <ROUTER>
