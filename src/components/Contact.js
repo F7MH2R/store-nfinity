567c2739ebf235f60915f000c3e2bff823c8217c
F7MH2R <skyicef7@gmail.com>
2023-09-09 09:16:15 -0600

    shet

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+// Contact.js
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contacto</h2>
+      {/* Contenido de la página de contacto */}
+    </div>
+  );
+}
+
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Error from "./components/Error";
+import Contact from "./Contact";
 
 ReactDOM.render(
   <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="*" element={<Error />} />
+      <Route path="/error" element={<Error />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   </HashRouter>,
   document.getElementById("root")
