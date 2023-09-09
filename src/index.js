535148082ce3bda17b419f3712542a89fb2e8413
F7MH2R <skyicef7@gmail.com>
2023-09-08 20:20:21 -0600

    arreglando esta chet de los router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { HashRouter as Router, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Error from "./components/Error";
 
-const router = createHashRouter([
+const router = Router([
   {
     path: "/",
     element: <App />,
     errorElement: <Error />,
   },
 ]);
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
