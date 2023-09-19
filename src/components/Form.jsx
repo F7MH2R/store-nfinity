94119ed68866620be556fcaff1ac7a9e10c1f040
leBryanXD <bryan.vasquez@catolica.edu.sv>
2023-09-19 01:32:12 -0600

    pagina de contacto lista?

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,8 +9,26 @@ const Form = () => {
           <input type="text" id="name" placeholder="Nombre-Apellidos" />
         </div>
         <div className="campo">
-          <label htmlFor="email">Ingresa tu nombre Completo</label>
+          <label htmlFor="email">Ingresa tu correo electrónico</label>
           <input type="email" id="email" placeholder="correo@correo.com" />
+        </div>
+        <div className="campo">
+          <label htmlFor="tel">Ingresa tu telefono</label>
+          <input type="text" id="tel" placeholder="0000-0000" />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="message">Mensaje</label>
+          <textarea
+            id="message"
+            placeholder="Escribe tu mensaje aquí"
+          ></textarea>
+        </div>
+        {/* Puedes agregar más campos aquí si es necesario */}
+
+        {/* Botón de envío del formulario */}
+        <div className="campo">
+          <button type="submit">Enviar</button>
         </div>
       </form>
     </>
